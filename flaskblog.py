886adf32ce96db4e63d51ec337bba57add46eb5e
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-09 01:47:09 +0300

    feat: added routes for about and home page

---
+++ flaskblog.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {'title':'kelvin blog',
+     'author':'Jane Dode',
+     'date_posted':'May 09, 2022'
+     },
+      {'title':'Jane blog',
+     'author':'Kelvin Dode',
+     'date_posted':'May 11, 2022'
+     }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html',posts=posts)
+
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html",title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
