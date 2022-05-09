33b3c1b39e64aaeffa0971cdbd9f714dcef3448f
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-09 10:47:18 +0300

    feat: validation for email and password for login and register forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template,url_for,flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -23,22 +23,30 @@ def home():
     return render_template('home.html',posts=posts)
 
 
-
 @app.route("/about")
 def about():
     return render_template("about.html",title='About')
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Successful! Account created for {form.username.data}.','success')
+        return redirect(url_for('home'))
     
     return render_template("register.html", title='Register',form=form)
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     
+    if form.validate_on_submit():
+        if form.email.data == 'kelvin.anjiku@student.moringaschool.com' and form.password.data == 'kevin':
+            flash('You are logged in successfully.', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed. Please check username and password!', 'danger')  
     return render_template("login.html", title='Login',form=form)
 
 if __name__ == '__main__':
