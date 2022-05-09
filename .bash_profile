22d4c9ecfe3b5fc6096d164c3294e14bcc9efb3b
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-10 02:58:45 +0300

    feat: created runtime file

---
+++ .bash_profile
@@ -0,0 +1,2 @@
+export SECRET_KEY='acd6b60c667a94557bce0aec3c409354'
+export SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get(' SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI= os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = 'acd6b60c667a94557bce0aec3c409354'
+    SQLALCHEMY_DATABASE_URI= 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
