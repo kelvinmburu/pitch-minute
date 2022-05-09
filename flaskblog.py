d161fdb7946a847575dde579c1e13293a104ce58
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-09 03:04:11 +0300

    chore: fixed typo

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template,url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'acd6b60c667a94557bce0aec3c409354'
 
 posts = [
-    {'title':'Jane Blog',
-     'author':'Jane Doe',
+    {'author':'Kelvin Mburu',
+     'title':'Blog Post 1',
+     'content':'First Post Content',
      'date_posted':'May 09, 2022'
      },
-      {'title':'Kelvin blog',
-     'author':'Kelvin Mburu',
-     'date_posted':'May 11, 2022'
+      {'author':'Kelvin Doe',
+     'title':'Blog Post 2',
+     'content':'Second Post Content',
+     'date_posted':'May 19, 2022'
      }
 ]
 
@@ -24,6 +27,19 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html",title='About')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    
+    return render_template("register.html", title='Register',form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    
+    return render_template("login.html", title='Login',form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
