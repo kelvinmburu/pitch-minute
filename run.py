e3fe6490869e5040ecd116e52c06f97529bf4477
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-10 11:22:37 +0300

    feat: installed flask-migrate

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
+from  flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager,Server
+from flask_sqlalchemy import SQLAlchemy
 from flaskblog import create_app
 
 app = create_app()
+
+manager = Manager(app)
+manager.add_command('server', Server)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     app.run(debug=True)
