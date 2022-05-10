d76136586c1e4da071357d12332839d019f3c91a
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-10 17:22:36 +0300

    feat: installed psycopg2

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,7 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = 'acd6b60c667a94557bce0aec3c409354'
-    SQLALCHEMY_DATABASE_URI= 'sqlite:///site.db'
+    uri = os.getenv("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = uri or 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
