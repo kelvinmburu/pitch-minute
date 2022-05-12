0434a3a6bdd60ead1d414fe2fd2c5e0a87d1c8f7
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-12 09:26:15 +0300

    feat: installed all dependecies

--- .bash_profile
+++ .bash_profile
@@ -1,2 +1,6 @@
 export SECRET_KEY='acd6b60c667a94557bce0aec3c409354'
-export SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:kevin@localhost/pitches'
+export MAIL_USERNAME='mburuhkelvin@gmail.com'
+export MAIL_PASSWORD='kj&murs1k$_22@09'
+
+python3.8 manage.py server
--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_migrate import Migrate
 from flaskblog.config import Config
 
 
@@ -14,6 +15,7 @@ login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
 mail = Mail()
+migrate = Migrate()
 
 
 
@@ -26,6 +28,7 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    migrate.init_app(app, db)
     
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
