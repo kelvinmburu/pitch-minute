c01652d3197bdc502ec7b6cfbd9fb7966117ba57
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-13 08:11:39 +0300

    heroku deployment

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -11,12 +11,12 @@ class Config:
     
     
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
+#    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
     
     
     
     
-DEBUG = True
+    DEBUG = True
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:kevin@localhost/pitches'
